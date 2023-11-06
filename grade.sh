3a78615667db48893927fea18c8dca4800998e8e
MohaElder169 <calen0909@hotmail.com>
2023-11-06 12:25:56 -0800

    .

--- grade.sh
+++ grade.sh
@@ -3,4 +3,7 @@ sudo apt-get install python
 mkdir students
 mkdir data
 cp *.txt students/
+cp chat-server/HandlerTests.java students
+cp chat-server/sesion.log students
 echo $1 > data/data.json
+python3 skill-demo.py .
