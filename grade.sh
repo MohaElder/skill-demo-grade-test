afe8ed078292e587cdaf184fddc24ea154ec9b7c
MohaElder169 <calen0909@hotmail.com>
2023-11-06 14:00:50 -0800

    .

--- grade.sh
+++ grade.sh
@@ -5,6 +5,6 @@ mkdir student/chat-server
 mkdir data
 cp *.txt student/
 cp chat-server/HandlerTests.java student/chat-server
-cp chat-server/sesion.log student/chat-server
+cp chat-server/session.log student/chat-server
 echo $1 > data/data.json
 python3 skill-demo-manual-grade/skill_demo.py .
